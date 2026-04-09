1d0201f595b8d304072897772e63fad5b62a924b
Sangram Patil <sangrampatil2290@gmail.com>
2026-04-09 18:39:54 +0530

    Adding arithmeticOperators

---
+++ arithmeticOperators.js
@@ -0,0 +1,28 @@
+//Arithmetic Operators
+
+let a=3;
+let b=2;
+let c=a+b;
+console.log(c); //5
+
+let d=a-b;
+console.log(d); //1
+
+let e=a*b;
+console.log(e); //6
+
+let f=a/b;
+console.log(f); //1.5
+let g=a%b;
+console.log(g); //1
+
+let h=a**b;
+console.log(h); //9
+let j=++a;
+console.log(j); //4
+let k=--b;
+console.log(k); //1
+
+
+
+
